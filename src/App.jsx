88c74f8100f494d8b31c5a401b2229959721ad9b
rsayyed591 <rehansayyed591@gmail.com>
2024-07-22 20:09:14 +0530

    updated http to https

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './assets/search.svg';
 
-console.log('API Key:', import.meta.env.VITE_MOVIES_API_KEY);
-// const API_URL = 'http://www.omdbapi.com?apikey=55b8c307'
-const API_URL = `https://www.omdbapi.com?apikey=${import.meta.env.VITE_MOVIES_API_KEY}`;
+const apikey = import.meta.env.VITE_MOVIES_API_KEY;
+
+const API_URL = `https://www.omdbapi.com?apikey=${apikey}`;
 
 function App() {
   const [movies, setMovies] = useState([]);
   
   const[searchTerm, setSearchTerm] = useState('');
   const searchMovies = async (title)=>{
-    const response = await fetch(`${API_URL}&s=${title}`);
+    const response = await fetch(`https://www.omdbapi.com?apikey=${apikey}&s=${title}`);
     const data = await response.json();
 
     setMovies(data.Search);
